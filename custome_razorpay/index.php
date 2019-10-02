fd5c3e566e115dc5911a7a44996357be0d928502
drapesourcecode <52990960+drapesourcecode@users.noreply.github.com>
2019-10-02 07:22:08 -0500

    Add files via upload

---
+++ custome_razorpay/index.php
@@ -0,0 +1,78 @@
+<?php
+
+require('config.php');
+require('razorpay-php/Razorpay.php');
+session_start();
+
+// Create the Razorpay Order
+
+use Razorpay\Api\Api;
+
+$api = new Api($keyId, $keySecret);
+
+//
+// We create an razorpay order using orders api
+// Docs: https://docs.razorpay.com/docs/orders
+//
+$orderData = [
+    'receipt'         => 3456,
+    'amount'          => 2000 * 100, // 2000 rupees in paise
+    'currency'        => 'INR',
+    'payment_capture' => 1 // auto capture
+];
+
+$razorpayOrder = $api->order->create($orderData);
+
+$razorpayOrderId = $razorpayOrder['id'];
+
+$_SESSION['razorpay_order_id'] = $razorpayOrderId;
+
+$displayAmount = $amount = $orderData['amount'];
+
+$data = [
+    "key"               => $keyId,
+    "amount"            => $amount,
+    "name"              => "DEBENDRA",
+    "description"       => "NGO Donate",
+    "image"             => "logo.png",
+    "prefill"           => [
+    "name"              => "Debendra Ku. Sahoo",
+    "email"             => "debendra@merchant.com",
+    "contact"           => "9999999999",
+    ],
+    "notes"             => [
+    "address"           => "Bhubaneswar",
+    "merchant_order_id" => "NGO_D1",
+    ],
+    "theme"             => [
+    "color"             => "#F37254"
+    ],
+    "order_id"          => $razorpayOrderId,
+];
+
+$json = json_encode($data);
+?>
+<!--  The entire list of Checkout fields is available at
+ https://docs.razorpay.com/docs/checkout-form#checkout-fields -->
+
+<form action="verify.php" method="POST">
+  <script
+    src="https://checkout.razorpay.com/v1/checkout.js"
+    data-key="<?php echo $data['key']?>"
+    data-amount="<?php echo $data['amount']?>"
+    data-currency="INR"
+    data-name="<?php echo $data['name']?>"
+    data-image="<?php echo $data['image']?>"
+    data-description="<?php echo $data['description']?>"
+    data-prefill.name="<?php echo $data['prefill']['name']?>"
+    data-prefill.email="<?php echo $data['prefill']['email']?>"
+    data-prefill.contact="<?php echo $data['prefill']['contact']?>"
+    data-notes.shopping_order_id="3456"
+    data-order_id="<?php echo $data['order_id']?>"
+    <?php if ($displayCurrency !== 'INR') { ?> data-display_amount="<?php echo $data['display_amount']?>" <?php } ?>
+    <?php if ($displayCurrency !== 'INR') { ?> data-display_currency="<?php echo $data['display_currency']?>" <?php } ?>
+  >
+  </script>
+  <!-- Any extra fields to be submitted with the form but not sent to Razorpay -->
+  <input type="hidden" name="shopping_order_id" value="3456">
+</form>
